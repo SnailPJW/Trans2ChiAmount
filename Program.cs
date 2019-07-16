59bb47158ddd5a5358dfdb9b3a80fc443f3d1db2
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-07-16 22:27:28 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ref : JS (https://gist.github.com/tonyc726/00c829a54a40cf80409f)
+namespace Trans2ChiAmount
+{
+    class Program
+    {
+        static string[] digit = {
+            "零","壹","貳","參","肆"
+           ,"伍","陸","柒","捌","玖"
+        };
+        //不規則陣列 宣告時直接初始化
+        static string[][] unit = {
+            new []{"元","萬","億" },
+            new []{"","拾","佰","仟"}
+        };
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Keyin an amount: ");
+                double amount;
+                double.TryParse(Console.ReadLine(), out amount);
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        static void ShiftRight(double number,int digit) {
+            //int.TryParse(digit, out digit);
+        }
+    }
+}
